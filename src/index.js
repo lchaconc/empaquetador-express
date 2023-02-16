3bcfc70ede7cdabfc99c5b8069c501b7ccb6972b
lchaconc <luis.chacon.campos@gmail.com>
2023-02-16 13:46:38 -0600

    prueba desployment -creacion de script start en package -eferenca de variable de entonro -instalaicón delibrería dotenv

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import express from 'express'
+import dotenv from "dotenv"
 
 const app = express()
-const port = 3500
+dotenv.config();
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
 })
