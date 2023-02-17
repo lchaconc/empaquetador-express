3bef59c6e4f8fba9ad2514c0ddf19a8c7ef945d1
lchaconc <luis.chacon.campos@gmail.com>
2023-02-17 12:54:12 -0600

    modificacion  de ruta para carga de html

--- src/app.js
+++ src/app.js
@@ -10,8 +10,8 @@ app.use(express.json());
 
 app.use( express.static('./public') );
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
+app.get("/", (req, res) => {    
+    res.sendFile(__dirname + "../public/index.html");
   });
   
 
