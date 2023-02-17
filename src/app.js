f7cbda865c6fc57f47009bb45eeceb156da93193
lchaconc <luis.chacon.campos@gmail.com>
2023-02-17 13:51:58 -0600

    imp de path en la ruta de index

--- src/app.js
+++ src/app.js
@@ -1,24 +1,19 @@
-import express from 'express'
-import dotenv from "dotenv"
-import indexRoutes from "./routes/index.routes"
-
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import indexRoutes from "./routes/index.routes";
 
 const app = express();
 dotenv.config();
 app.set("port", process.env.PORT || 3700);
 app.use(express.json());
 
-app.use( express.static('./public') );
+app.use(express.static("./public"));
 
-app.get("/", (req, res) => {    
-    res.sendFile(__dirname + "./public/index.html");
-  });
-  
-
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.use(indexRoutes);
-
-
-
 
 export default app;
